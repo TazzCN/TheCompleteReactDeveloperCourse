4171a98f7f3b2be464ebacbdbf4c65cd2f616fdf
TazzCN <tazzcn@gmail.com>
2019-07-23 12:30:18 +0100

    23/07/19
    Logical Operators and JSX Basics

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,79 @@
-"use strict";
+'use strict';
 
 console.log('App.js is running');
 
 //JSX
 
+var App = {
+    title: ' Indecision App',
+    subTitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "p",
-  null,
-  "Does this change"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        App.title
+    ),
+    App.subTitle && React.createElement(
+        'p',
+        null,
+        App.subTitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        App.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+
+var user = {
+    age: 10
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,41 @@ console.log('App.js is running');
 
 //JSX
 
- var template =  <p>Does this change</p>;
+var App = {
+    title: ' Indecision App',
+    subTitle: 'Put your life in the hands of a computer',
+    options:['One', 'Two']
+};
+
+ var template = ( 
+    <div>
+        <h1>{App.title}</h1>
+        {App.subTitle && <p>{App.subTitle}</p>}
+        <p>{App.options.length > 0 ? "Here are your options": "No options"}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+ );
+
+ var user = {
+     age: 10
+ };
+
+ function getLocation(location) {
+     if (location){
+         return <p>Location: {location}</p>;
+     }
+ }
+
+ var templateTwo = (
+     <div>
+         <h1>{user.name?user.name:'Anonymous'}</h1>
+         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+         {getLocation(user.location)}
+     </div>
+ );
 
  var appRoot = document.getElementById("app");
 
