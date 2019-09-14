a58f11b3cca66f1f9fd0a5bcfd69d4c6a3126eac
TazzCN <tazzcn@gmail.com>
2019-09-14 18:48:13 +0100

    ES6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,8 @@
+import {square, add} from './utils.js';
+import { canDrink,isAdult } from './person.js';
 console.log('app.js is running!!!');
+
+console.log(square(4));
+console.log(add(1,2));
+console.log(isAdult(20));
+console.log(canDrink(20));
