b442b326609f1e1c1c04bc6b11822464ffa112b5
TazzCN <tazzcn@gmail.com>
2019-09-24 07:02:53 +0100

    ES6 Spread Operator in Reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,9 +1,37 @@
 import {createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const addExpense = (
+    {
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0
+}= {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 
 const expensesReducerDefaultState = [];
 
 const expenseReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [...state,
+                action.expense];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
         default:
             return state;
     }
@@ -29,6 +57,11 @@ const store = createStore(
         filter: filterReducer
     })
 );
+
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300}));
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 console.log(store.getState());
 
