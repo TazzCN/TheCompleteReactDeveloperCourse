48fe9e557938665223b8dce7a089e2873181891a
TazzCN <tazzcn@gmail.com>
2019-10-24 07:13:19 +0100

    Production Webpack

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -21,8 +21,8 @@ export class AddExpensePage extends React.Component {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => ({
   addExpense: (expense) => dispatch(addExpense(expense))
-};
+});
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
